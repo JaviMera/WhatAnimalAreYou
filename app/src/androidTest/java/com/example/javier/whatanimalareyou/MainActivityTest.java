73532288bddc277e6994bc2fb23391af412dc95d
Javier Mera <Javier@JaviLapchap.(none)>
2016-10-04 17:31:07 -0500

    Added hard coded string array to represent the choices that the user will see for each statement. Added android test for the spinner view in the main activity, and finally got to show all the available choices in the spinner.

---
+++ app/src/androidTest/java/com/example/javier/whatanimalareyou/MainActivityTest.java
@@ -0,0 +1,57 @@
+package com.example.javier.whatanimalareyou;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.Spinner;
+
+import com.example.javier.whatanimalareyou.ui.MainActivity;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by Javier on 10/4/2016.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void activityCreateSpinnerIsVisible() throws Exception {
+
+        // Assert
+        onView(withId(R.id.choiceSpinnerView)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void activityCreateSpinnerHasCorrectChoice() throws Exception {
+
+        // Arrange
+        String[] choices = activityRule.getActivity().getResources().getStringArray(R.array.choices_array);
+
+        // Act
+        onView(withId(R.id.choiceSpinnerView)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(choices[0]))).perform(click());
+
+        // Assert
+        onView(withId(R.id.choiceSpinnerView)).check(matches(withSpinnerText(choices[0])));
+    }
+}
--- app/src/main/java/com/example/javier/whatanimalareyou/ui/MainActivity.java
+++ app/src/main/java/com/example/javier/whatanimalareyou/ui/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.javier.whatanimalareyou.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.javier.whatanimalareyou.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> mChoices;
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] choicesArray = getResources().getStringArray(R.array.choices_array);
+        mChoices = new ArrayList<>(Arrays.asList(choicesArray));
+
+        mSpinner = (Spinner)findViewById(R.id.choiceSpinnerView);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.choices_array, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(spinnerAdapter);
     }
 }
