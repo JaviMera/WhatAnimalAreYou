8f93bc03f396d6e5ecb158cf0ccf04321e8e62f6
Javier Mera <Javier@JaviLapchap.(none)>
2016-10-04 23:05:44 -0500

    Added statement and statement count textviews to main activity, and added their respective view styles as well. Adjusted the spinner view, by including a bitmap for the arrow and a custom drawable to paint the background of it. Also applied the custom font to the text views

--- app/src/main/java/com/example/javier/whatanimalareyou/ui/MainActivity.java
+++ app/src/main/java/com/example/javier/whatanimalareyou/ui/MainActivity.java
@@ -3,34 +3,43 @@ package com.example.javier.whatanimalareyou.ui;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.javier.whatanimalareyou.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner mSpinner;
+    private Spinner mChoiceSpinner;
+    private TextView mStatementTextView;
+    private TextView mStatementsCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/LuckiestGuy.ttf");
+
+        mStatementTextView = (TextView)findViewById(R.id.statementTextView);
+        mStatementTextView.setTypeface(font);
+
+        mStatementsCountTextView = (TextView)findViewById(R.id.statementCountTextView);
+        mStatementsCountTextView.setTypeface(font);
+
         String[] choicesArray = getResources().getStringArray(R.array.choices_array);
 
-        mSpinner = (Spinner)findViewById(R.id.choiceSpinnerView);
+        mChoiceSpinner = (Spinner)findViewById(R.id.choiceSpinnerView);
         ChoiceSpinnerAdapter spinnerAdapter = new ChoiceSpinnerAdapter(
             this,
             android.R.layout.simple_spinner_item,
-            Typeface.createFromAsset(getAssets(), "fonts/LuckiestGuy.ttf"),
+            font,
             new ArrayList<>(Arrays.asList(choicesArray))
             );
 
-        mSpinner.setAdapter(spinnerAdapter);
+        mChoiceSpinner.setAdapter(spinnerAdapter);
     }
 }
